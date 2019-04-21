701cd2705d9e302e973aca756a07c506c11c69ad
Lom-Ali <lomaliyakubov@gmail.com>
2019-04-21 12:32:13 +0300

    Refactor 'polls' app view

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.polls_list, name='polls'),
+    path('', views.PollsList.as_view(), name='polls'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,12 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from polls.models import Poll
 from polls.serializers import PollSerializer
+from rest_framework import generics
+
+class PollsList(generics.ListCreateAPIView):
+    """Create the Poll and get all the Polls"""
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
 
 
-@api_view(['GET', 'POST'])
-def polls_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
-        polls = Poll.objects.all()
-        serializer = PollSerializer(polls, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = PollSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
