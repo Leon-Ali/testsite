24a3786cdfce563b6c8cbbf51b6c16728b24c55a
Lom-Ali <lomaliyakubov@gmail.com>
2019-04-21 13:05:33 +0300

    Add models and serializers

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Poll(models.Model):
     name = models.CharField(max_length=200, default='untitled')
+
+
+
+class Question(models.Model):
+    MOVIES = 'MV'
+    GAMES = 'GA'
+    BOOKS = 'BO'
+
+    GROUP_CHOICES = (
+        (MOVIES, 'movies'),
+        (GAMES, 'games'),
+        (BOOKS, 'books')
+    )
+
+    header = models.CharField(max_length=200)
+    order = models.IntegerField()
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    group = models.CharField(max_length=2, choices=GROUP_CHOICES)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    order = models.IntegerField()
+
+
+class FinishedPoll(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
 
--- polls/serializers.py
+++ polls/serializers.py
@@ -1,4 +1,4 @@
-from .models import Poll
+from .models import Poll, Question, Answer, FinishedPoll
 from rest_framework import serializers
 
 
@@ -7,3 +7,42 @@ class PollSerializer(serializers.ModelSerializer):
     class Meta:
         model = Poll
         fields = ('id', 'name')
+
+
+class ChoicesField(serializers.Field):
+    def __init__(self, choices, **kwargs):
+        self._choices = choices
+        super(ChoicesField, self).__init__(**kwargs)
+
+    def to_representation(self, obj):
+        for i in self._choices:
+            if obj in i:
+                return i[-1]
+
+
+    def to_internal_value(self, data):
+        for i in self._choices:
+            if data in i:
+                return i[0]
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    group = ChoicesField(choices=Question.GROUP_CHOICES, required=True)
+
+    class Meta:
+        model = Question
+        fields = ('id', 'header', 'order', 'group')
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Answer
+        fields = ('id', 'text', 'order')
+
+
+class FinishedPollSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = FinishedPoll
+        fields = ('id', 'user', 'answer')
