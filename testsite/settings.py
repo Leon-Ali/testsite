8589ec223380e99ed3b5640e3542e2dfceff8260
Lom-Ali <lomaliyakubov@gmail.com>
2019-06-10 20:30:03 +0300

    Add login redirect

--- testsite/settings.py
+++ testsite/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
+LOGIN_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
